37e0a6b7cb035363d3ac31df21fb4def05541b9e
MatiasIvanM <matiasivan.sd@gmail.com>
2023-07-18 21:59:57 -0300

    FIX temperament

--- api/src/Controller/getTemperament.js
+++ api/src/Controller/getTemperament.js
@@ -1,8 +1,8 @@
 const axios = require ('axios');
 const { Temperament } = require ('../db');
 const {API_KEY, URL} = process.env;
-
-  //ERROR NOT NULL VIOLATION
+ 
+/*   //ERROR NOT NULL VIOLATION
   const getTemperament = async (req, res) => {
     try {
       // Buscar en la base de datos
@@ -22,7 +22,7 @@ const {API_KEY, URL} = process.env;
           if (temperamentFinal.indexOf(elem) < 0) temperamentFinal.push(elem);
         });
         
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 0; i++) {
           await Temperament.create({
             name: temperamentFinal[i],
           });
@@ -34,10 +34,46 @@ const {API_KEY, URL} = process.env;
       res.status(404).send({ error: error.message });
     }
   };
-
-module.exports = getTemperament;
-
-
+ */
+const getTemperaments = async (req, res) => {
+    try {
+      // Buscar en la base de datos
+      let dbTemperaments = await Temperament.findAll();
+  
+      if (dbTemperaments.length !== 0) {
+        res.send(dbTemperaments);
+      } else {
+        const response = await axios.get(`${URL}?api_key=${API_KEY}`);
+        const breeds = response.data;
+  
+        const temperaments = new Set();
+  
+        breeds.forEach((breed) => {
+          if (breed.temperament) {
+            const breedTemperaments = breed.temperament.split(", ");
+            breedTemperaments.forEach((temperament) => {
+              temperaments.add(temperament);
+            });
+          }
+        });
+  
+        const temperamentList = Array.from(temperaments);
+  
+        const createdTemperaments = await Temperament.bulkCreate(
+          temperamentList.map((name) => ({ name }))
+        );
+  
+        res.status(200).send(createdTemperaments);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: "Error al obtener los temperamentos" });
+    }
+  };
+  
+  module.exports = getTemperaments;
+ 
+  
 //MODELO ANTERIOR SE SUPLANTA PARA EXTRAER Y CREAR DATOS
 /*  const apiTemp = await axios.get(`${URL}?api_key=${API_KEY}`);
  let apiMapTemp = apiTemp.data.result;
